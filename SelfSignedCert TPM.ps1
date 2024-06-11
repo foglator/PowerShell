a27bf42528cabc4eaf20934358d56be7cfe1488e
Omiii <roman@dosol.cz>
2024-06-11 22:39:14 +0200

    SelfSignedCert TPM

---
+++ SelfSignedCert TPM.ps1
@@ -0,0 +1,16 @@
+install-module microsoft.graph -Scope CurrentUser
+
+Connect-MgGraph -Certificate xxxx
+
+[X509Certificate] $cer = New-SelfSignedCertificate `
+    -Subject "CN=$env:COMPUTERNAME Entra ID Script Authentication" `
+    -KeyAlgorithm RSA `
+    -KeyLength 2048 `
+    -CertStoreLocation Cert:\LocalMachine\My `
+    -KeyExportPolicy NonExportable `
+    -Provider 'Microsoft Platform Key Storage Provider' `
+    -NotAfter (Get-Date).AddYears(1)
+Export-Certificate -Type CERT -Cert $cer -FilePath 'C:\AzureScriptAuthentication.cer'  -Force 
+
+dir Cert:\LocalMachine\My | fl
+dir Cert:\CurrentUser\My | fl
