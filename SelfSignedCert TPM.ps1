c8195a1f1cdc828c0579d434d95c546c9d884606
foglator <67166653+foglator@users.noreply.github.com>
2024-08-17 22:48:06 +0200

    update

--- SelfSignedCert TPM.ps1
+++ SelfSignedCert TPM.ps1
@@ -1,6 +1,4 @@
-install-module microsoft.graph -Scope CurrentUser
-
-Connect-MgGraph -Certificate xxxx
+# Create a self-signed certificate, store in TPM
 
 [X509Certificate] $cer = New-SelfSignedCertificate `
     -Subject "CN=$env:COMPUTERNAME Entra ID Script Authentication" `
@@ -12,5 +10,7 @@ Connect-MgGraph -Certificate xxxx
     -NotAfter (Get-Date).AddYears(1)
 Export-Certificate -Type CERT -Cert $cer -FilePath 'C:\AzureScriptAuthentication.cer'  -Force 
 
-dir Cert:\LocalMachine\My | fl
-dir Cert:\CurrentUser\My | fl
+#dir Cert:\LocalMachine\My | fl
+#dir Cert:\CurrentUser\My | fl
+#install-module microsoft.graph -Scope CurrentUser
+#Connect-MgGraph -Certificate xxxx
